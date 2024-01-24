2cf20c346bb065e3b17f50d0da6e48fc8643fea6
aminiyolo <aminiyolo@gmail.com>
2024-01-25 00:46:16 +0900

    footer 추가

---
+++ components/footer/footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className='absolute bottom-0 w-full text-center p-3 bg-color text-white rounded-t-sm'>
+      <div>2024 Minyong Park - All rights reserved.</div>
+    </footer>
+  );
+}
+
+export default Footer;
