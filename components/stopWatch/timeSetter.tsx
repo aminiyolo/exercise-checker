e37256a2201ff353f8b6d3b0c32a8ff5b17b1f3f
aminiyolo <aminiyolo@gmail.com>
2024-02-05 15:56:26 +0900

    Timer 시간 설정 컴포넌트 추가

---
+++ components/stopWatch/timeSetter.tsx
@@ -0,0 +1,45 @@
+import React, { Dispatch, MutableRefObject, SetStateAction } from 'react';
+
+const commonStyle =
+  'border-solid border-b-2 border-black w-12 text-right focus:outline-none';
+
+interface Props {
+  setMinutes: Dispatch<SetStateAction<number>>;
+  setSeconds: Dispatch<SetStateAction<number>>;
+  isProcessing: boolean;
+  min: MutableRefObject<number>;
+  sec: MutableRefObject<number>;
+}
+
+function TimeSetter({ setMinutes, setSeconds, isProcessing, min, sec }: Props) {
+  return (
+    <div className='flex flex-row'>
+      <input
+        min={0}
+        type='number'
+        className={commonStyle}
+        disabled={isProcessing}
+        onChange={(e) => {
+          const value = Number(e.target.value);
+          setMinutes(value);
+          min.current = value;
+        }}
+      />
+      <span className='mx-2'>분</span>
+      <input
+        min={0}
+        type='number'
+        className={commonStyle}
+        disabled={isProcessing}
+        onChange={(e) => {
+          const value = Number(e.target.value);
+          setSeconds(value);
+          sec.current = value;
+        }}
+      />
+      <span className='ml-2'>초</span>
+    </div>
+  );
+}
+
+export default TimeSetter;
