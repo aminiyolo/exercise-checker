83e7f13853c231d16756ed7526bc8f2890095fe5
aminiyolo <aminiyolo@gmail.com>
2024-02-07 15:15:58 +0900

    constant 추가

---
+++ constant.ts
@@ -0,0 +1,7 @@
+export const EXERCISE = [
+  { name: '푸시업', type: '횟수' },
+  { name: '유산소', type: '시간' },
+  { name: '스쿼트', type: '횟수' },
+  { name: '풀업', type: '횟수' },
+  { name: '스트레칭', type: '시간' },
+] as const;
